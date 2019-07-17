b4eba949fd09d0f2af861a88e0b2dae9120d028c
khchine5 <hamzakhchine@gmail.com>
2019-07-17 19:19:26 +0100

    #3036 Add db and user creating

--- getlino.py
+++ getlino.py
@@ -3,7 +3,11 @@
 # License: BSD (see file COPYING for details)
 #
 
-import os, sys, stat, shutil, grp
+import os
+import sys
+import stat
+import shutil
+import grp
 import configparser
 import virtualenv
 import click
@@ -28,29 +32,39 @@ user = root
 
 DB_ENGINES = [
     DbEngine('pgsql', "postgresql postgresql-contrib", "psycopg2-binary"),
-    DbEngine('mysql', "mysql-server libmysqlclient-dev python-dev libffi-dev libssl-dev", "mysqlclient"),
+    DbEngine(
+        'mysql', "mysql-server libmysqlclient-dev python-dev libffi-dev libssl-dev", "mysqlclient"),
     DbEngine('sqlite', "", "")
 ]
 
 KNOWN_APPS = [
-  KnownApp("noi", "lino_noi.lib.noi.settings", "https://github.com/lino-framework/noi"),
-  KnownApp("voga", "lino_voga.lib.voga.settings", "https://github.com/lino-framework/voga"),
-  KnownApp("cosi", "lino_cosi.lib.cosi.settings", "https://github.com/lino-framework/cosi"),
-  KnownApp("avanti", "lino_avanti.lib.avanti.settings", "https://github.com/lino-framework/avanti"),
-  KnownApp("weleup", "lino_weleup.lib.weleup.settings", "https://github.com/lino-framework/weleup"),
-  KnownApp("welcht", "lino_voga.lib.voga.settings", "https://github.com/lino-framework/welcht"),
-  KnownApp("min2", "lino_book.projects.min2.settings", "https://github.com/lino-framework/book"),
+    KnownApp("noi", "lino_noi.lib.noi.settings",
+             "https://github.com/lino-framework/noi"),
+    KnownApp("voga", "lino_voga.lib.voga.settings",
+             "https://github.com/lino-framework/voga"),
+    KnownApp("cosi", "lino_cosi.lib.cosi.settings",
+             "https://github.com/lino-framework/cosi"),
+    KnownApp("avanti", "lino_avanti.lib.avanti.settings",
+             "https://github.com/lino-framework/avanti"),
+    KnownApp("weleup", "lino_weleup.lib.weleup.settings",
+             "https://github.com/lino-framework/weleup"),
+    KnownApp("welcht", "lino_voga.lib.voga.settings",
+             "https://github.com/lino-framework/welcht"),
+    KnownApp("min2", "lino_book.projects.min2.settings",
+             "https://github.com/lino-framework/book"),
 ]
 
 APPNAMES = [a.name for a in KNOWN_APPS]
 
-CONF_FILES = ['/etc/getlino/getlino.conf', os.path.expanduser('~/.getlino.conf')]
+CONF_FILES = ['/etc/getlino/getlino.conf',
+              os.path.expanduser('~/.getlino.conf')]
 CONFIG = configparser.ConfigParser()
 FOUND_CONFIG_FILES = CONFIG.read(CONF_FILES)
 DEFAULTSECTION = CONFIG[CONFIG.default_section]
 
 
 CONFIGURE_OPTIONS = []
+
 
 def add(spec, default, help, type=None):
 
@@ -62,21 +76,52 @@ def add(spec, default, help, type=None):
     o.default = DEFAULTSECTION.get(o.name, default)
     CONFIGURE_OPTIONS.append(o)
 
+
 # must be same order as in signature of configure command below
 add('--projects-root', '/usr/local/lino', 'Base directory for Lino sites')
 add('--backups-root', '/var/backups/lino', 'Base directory for backups')
 add('--log-root', '/var/log/lino', 'Base directory for log files')
 add('--usergroup', 'www-data', "User group for files to be shared with the web server")
-add('--supervisor-dir', '/etc/supervisor/conf.d', "Directory for supervisor config files")
-add('--db-engine', 'mysql', "Default database engine for new sites.", click.Choice([e.name for e in DB_ENGINES]))
+add('--supervisor-dir', '/etc/supervisor/conf.d',
+    "Directory for supervisor config files")
+add('--db-engine', 'sqlite', "Default database engine for new sites.",
+    click.Choice([e.name for e in DB_ENGINES]))
 add('--repos-dir', 'repositories', "Default repositories directory for new sites")
 add('--env-dir', 'env', "Default virtualenv directory for new sites")
 add('--appy/--no-appy', True, "Whether to use appypod and LibreOffice")
 add('--redis/--no-redis', True, "Whether to use appypod and LibreOffice")
-add('--devtools/--no-devtools', False, "Whether to use developer tools (build docs and run tests)")
+add('--devtools/--no-devtools', False,
+    "Whether to use developer tools (build docs and run tests)")
 add('--once/--no-once', False, "Setup a temporary server for a single test run")
 add('--admin-name', 'Joe Dow', "The full name of the server maintainer")
-add('--admin-email', 'joe@example.com', "The email address of the server maintainer")
+add('--admin-email', 'joe@example.com',
+    "The email address of the server maintainer")
+
+
+def create_database_user(user, pwd, db_engine):
+    if db_engine == 'mysql':
+        sub_command = "create user '{}'@'localhost' identified by '{}';".format(
+            user, pwd)
+        command = 'sudo mysql -uroot -e "{};"'.format(sub_command)
+    elif db_engine == 'pgsql':
+        sub_command = "psql -c \"CREATE USER {} WITH PASSWORD '{}';\"".format(
+            user, pwd)
+        command = 'sudo -u postgres bash -c "{};"'.format(sub_command)
+    if command:
+        os.system(command)
+
+
+def create_database(user, database, db_engine):
+    if db_engine == 'mysql':
+        sub_command = "create database {} charset 'utf8'; grant all on {database}.* to {} with grant option;".format(
+            database, user)
+        command = 'mysql -uroot -e "{};"'.format(sub_command)
+    elif db_engine == 'pgsql':
+        sub_command = "CREATE DATABASE {}; GRANT ALL PRIVILEGES ON DATABASE {} TO {};".format(
+            database, user)
+        command = 'sudo -u postgres bash -c "{};"'.format(sub_command)
+    if command:
+        os.system(command)
 
 
 def check_permissions(pth, batch=True, executable=False):
@@ -100,7 +145,7 @@ def check_permissions(pth, batch=True, executable=False):
     if imode ^ mode:
         msg = "Set mode for {} from {} to {}".format(
             pth, imode, mode)
-            # pth, stat.filemode(imode), stat.filemode(mode))
+        # pth, stat.filemode(imode), stat.filemode(mode))
         if batch or click.confirm(msg, default=True):
             os.chmod(pth, mode)
 
@@ -126,6 +171,7 @@ def run_in_env(env, cmd):
     cmd = ". {}/bin/activate && {}".format(env, cmd)
     os.system(cmd)
 
+
 def install_python_requirements_old():
     command = """
     pip3 install -U pip setuptools
@@ -140,7 +186,8 @@ def install(packages, sys_executable=None):
         command = ". {}/bin/activate".format(sys_executable)
         os.system(command)
         for package in packages.split(' '):
-            subprocess.call(["{}/bin/python".format(sys_executable), "-m", "pip", "install", package])
+            subprocess.call(
+                ["{}/bin/python".format(sys_executable), "-m", "pip", "install", package])
     else:
         subprocess.call([sys.executable, "-m", "pip", "install", packages])
 
@@ -159,6 +206,7 @@ def yes_or_no(msg, yes="yY", no="nN"):
 
 # This will be decorated below. We cannot use decorators because we define the
 # list of options in CONFIGURE_OPTIONS
+
 
 def configure(ctx, batch,
               projects_root, backups_root, log_root, usergroup,
@@ -212,12 +260,13 @@ def configure(ctx, batch,
     else:
         raise click.Abort()
 
+
 params = [
     click.Option(['--batch/--no-batch'], default=False, help=BATCH_HELP)
 ] + CONFIGURE_OPTIONS
 configure = click.pass_context(configure)
-configure = click.Command('configure', callback=configure, params=params, help=configure.__doc__)
-
+configure = click.Command('configure', callback=configure,
+                          params=params, help=configure.__doc__)
 
 
 @click.command()
@@ -247,7 +296,8 @@ def setup(ctx, batch):
         os.system("apt-get upgrade")
 
     if batch or click.confirm("Install required system packages"):
-        apt_install("git subversion python3 python3-dev python3-setuptools python3-pip supervisor")
+        apt_install(
+            "git subversion python3 python3-dev python3-setuptools python3-pip supervisor")
         apt_install("nginx")
         apt_install("monit")
 
@@ -260,6 +310,8 @@ def setup(ctx, batch):
         for e in DB_ENGINES:
             if DEFAULTSECTION.get('db_engine') == e.name:
                 apt_install(e.apt_packages)
+            if DEFAULTSECTION.get('db_engine') == 'mysql':
+                os.system("sudo mysql_secure_installation")
         if DEFAULTSECTION.get('appy'):
             apt_install("libreoffice python3-uno")
 
@@ -275,7 +327,6 @@ def setup(ctx, batch):
                 os.system("service {} restart".format(srv))
 
     click.echo("Lino server setup completed.")
-
 
 
 @click.command()
@@ -300,10 +351,6 @@ def install_python_requirements(ctx, env):
     run_in_env(env, "pip install appy")
 
 
-
-
-
-
 @click.command()
 @click.argument('appname', metavar="APPNAME", type=click.Choice(APPNAMES))
 @click.argument('prjname')
@@ -313,7 +360,7 @@ def install_python_requirements(ctx, env):
               help="Whether to use development version of the application")
 @click.pass_context
 def startsite(ctx, appname, prjname,
-              dev,server_url,
+              dev, server_url,
               admin_full_name='Joe Dow',
               admin_email='joe@example.com',
               db_engine='sqlite',
@@ -330,31 +377,24 @@ def startsite(ctx, appname, prjname,
 
     PRJNAME : The project name for the new site.
 
-    """ # .format(appnames=' '.join(APPNAMES))
+    """  # .format(appnames=' '.join(APPNAMES))
     if len(FOUND_CONFIG_FILES) == 0:
-        raise click.UsageError("This server is not yet configured. Did you run `sudo getlino.py configure`?")
+        raise click.UsageError(
+            "This server is not yet configured. Did you run `sudo getlino.py configure`?")
 
     prjpath = os.path.join(DEFAULTSECTION.get('projects_root'), prjname)
     if os.path.exists(prjpath):
         raise click.UsageError("Project directory {} already exists.")
 
-    #raise Exception("Sorry, this command is not yet fully implemented")
-
     projects_root = DEFAULTSECTION.get('projects_root')
     env_dir = DEFAULTSECTION.get('env_dir')
     db_engine = DEFAULTSECTION.get('db_engine')
-    full_envdir = os.path.join(projects_root,prjname,env_dir)
-    project_dir = os.path.join(projects_root,prjname)
+    full_envdir = os.path.join(projects_root, prjname, env_dir)
+    project_dir = os.path.join(projects_root, prjname)
     repos_dir = DEFAULTSECTION.get('repos_dir')
-    full_repos_dir = os.path.join(full_envdir,repos_dir)
-    # envdir = config['LINO']['envdir']
+    full_repos_dir = os.path.join(full_envdir, repos_dir)
 
     if not no_input:
-        if not click.confirm("virtualenv directory : {}".format(full_envdir), default=True):
-            print("virtualenv directory")
-            answer = input()
-            if len(answer):
-                full_envdir = answer
 
         if not click.confirm("Project name : {} ".format(prjname), default=True):
             print("Project name :")
@@ -374,15 +414,13 @@ def startsite(ctx, appname, prjname,
             if len(answer):
                 prjname = answer
 
-            
-
-        #if not click.confirm("Application git repo  : {} ".format(app_git_repo), default=True):
+        # if not click.confirm("Application git repo  : {} ".format(app_git_repo), default=True):
         #    print("Application git repo :")
         #    answer = input()
         #    if len(answer):
         #        app_git_repo = answer
 
-        #if not click.confirm("Application setting  : {} ".format(app_settings), default=True):
+        # if not click.confirm("Application setting  : {} ".format(app_settings), default=True):
         #    print("Application setting :")
         #    answer = input()
         #    if len(answer):
@@ -405,18 +443,18 @@ def startsite(ctx, appname, prjname,
             answer = input()
             if len(answer):
                 admin_email = answer
+        if db_engine != "sqlite":
+            if not click.confirm("db user  : {} ".format(db_user), default=True):
+                print("db user :")
+                answer = input()
+                if len(answer):
+                    db_user = answer
 
-        if not click.confirm("db user  : {} ".format(db_user), default=True):
-            print("db user :")
-            answer = input()
-            if len(answer):
-                db_user = answer
-
-        if not click.confirm("db password  : {} ".format(db_password), default=True):
-            print("db password :")
-            answer = input()
-            if len(answer):
-                db_password = answer
+            if not click.confirm("db password  : {} ".format(db_password), default=True):
+                print("db password :")
+                answer = input()
+                if len(answer):
+                    db_password = answer
 
     app_git_repo = KNOWN_APPS[APPNAMES.index(appname)].git_repo
     app_settings = KNOWN_APPS[APPNAMES.index(appname)].settings_module
@@ -428,8 +466,8 @@ def startsite(ctx, appname, prjname,
 
     extra_context = {
         "prjname": prjname,
-        "projects_root":projects_root,
-        "reposdir":repos_dir,
+        "projects_root": projects_root,
+        "reposdir": repos_dir,
         "appname": appname,
         "app_git_repo": app_git_repo,
         "app_package": app_package,
@@ -447,67 +485,73 @@ def startsite(ctx, appname, prjname,
     }
     usergroup = DEFAULTSECTION.get('usergroup')
 
-    out = subprocess.Popen(['groups | grep ' + usergroup], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+    out = subprocess.Popen(['groups | grep ' + usergroup],
+                           stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
     stdout, stderr = out.communicate()
     if str(stdout):
         print("OK you belong to the {0} user group.".format(usergroup))
     else:
-        print("ERROR: you don't belong to the {0} user group.".format(usergroup))
+        print(
+            "ERROR: you don't belong to the {0} user group.".format(usergroup))
         print("Maybe you want to run:")
         # echo sudo usermod -a -G $USERGROUP `whoami`
         print("echo sudo adduser `whoami` {0}".format(usergroup))
         return
 
-    print('Creating a new production site into {0} using Lino {1} ...'.format(projects_root, appname))
+    print('Creating a new production site into {0} using Lino {1} ...'.format(
+        projects_root, appname))
 
     #os.system('mkdir {0}'.format(projects_root))
     #os.system('cd {0}'.format(projects_root))
     #sys_executable = os.path.join(os.path.expanduser(projects_root), envdir)
-    #print(full_envdir)
+    # print(full_envdir)
     #command = ". {}/bin/activate".format(full_envdir)
-    #os.system(command)
+    # os.system(command)
     #os.system('cd {0}'.format(projects_root))
     # os.system("cookiecutter https://github.com/lino-framework/cookiecutter-startsite")
-    
+    current_umask = os.umask(0o002)
+    # os.umask(current_umask)
+    print("current_umask {}".format(current_umask))
     cookiecutter(
         "https://github.com/lino-framework/cookiecutter-startsite",
-        no_input=True, extra_context=extra_context,output_dir=projects_root)
+        no_input=True, extra_context=extra_context, output_dir=projects_root)
 
     create_virtualenv(full_envdir)
     for e in DB_ENGINES:
         if DEFAULTSECTION.get('db_engine') == e.name:
-            run_in_env(full_envdir,"pip install {}".format(e.python_packages))
+            run_in_env(full_envdir, "pip install {}".format(e.python_packages))
     if not os.path.exists(full_repos_dir):
-            os.makedirs(full_repos_dir, exist_ok=True)
+        os.makedirs(full_repos_dir, exist_ok=True)
     os.chdir(full_repos_dir)
 
-    if dev or True:
+    if dev:
         os.system("git clone https://github.com/lino-framework/lino")
-        run_in_env(full_envdir,"pip install -e lino")
+        run_in_env(full_envdir, "pip install -e lino")
         os.system("git clone https://github.com/lino-framework/xl")
-        run_in_env(full_envdir,"pip install -e xl")
+        run_in_env(full_envdir, "pip install -e xl")
 
     if app_git_repo:
         os.system("git clone {}".format(app_git_repo))
-        run_in_env(full_envdir,"pip install -e {}".format(app_package_name))
+        run_in_env(full_envdir, "pip install -e {}".format(app_package_name))
     else:
-        run_in_env(full_envdir,"pip install {}".format(app_package_name))
-
+        run_in_env(full_envdir, "pip install {}".format(app_package_name))
 
     run_in_env(full_envdir, "pip install -U uwsgi")
-    run_in_env(full_envdir, "pip install -U svn+https://svn.forge.pallavi.be/appy-dev/dev1#egg=appy")
+    run_in_env(
+        full_envdir, "pip install -U svn+https://svn.forge.pallavi.be/appy-dev/dev1#egg=appy")
     os.chdir(project_dir)
+    if db_engine != "sqlite":
+        create_database_user(db_user, db_password, db_engine)
+        create_database(db_user, prjname, db_engine)
     prep_command = "python manage.py prep --noinput"
     print(prep_command)
-    run_in_env(full_envdir,prep_command)
-    #Testing 
-    #cookiecutter(
-    #    "/media/khchine5/011113a1-84fe-48ef-826d-4c81de9456731/home/khchine5/PycharmProjects/lino/cookiecutter-startsite",
-    #    no_input=True, extra_context=extra_context)
+    run_in_env(full_envdir, prep_command)
+
 
 @click.group()
 def main():
     pass
+
 
 main.add_command(configure)
 main.add_command(install_python_requirements)
@@ -517,5 +561,3 @@ main.add_command(startsite)
 if __name__ == '__main__':
     main()
     # main(auto_envvar_prefix='GETLINO')
-
-
